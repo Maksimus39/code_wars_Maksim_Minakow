8f11267a6b0f5e8cdc68219d18f8eb5d5ab208ae
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-01-29 23:40:01 -0800

    Учитывая два целочисленных массива a, b, оба длины >= 1, создайте программу, которая возвращает true, если сумма квадратов каждого элемента в a строго больше суммы кубов каждого элемента в b.

---
+++ 8 kyu/main6.js
@@ -0,0 +1,12 @@
+// Учитывая два целочисленных массива a, b, оба длины >= 1, создайте программу, которая возвращает true, если сумма квадратов каждого элемента в a строго больше суммы кубов каждого элемента в b.
+
+
+function arrayMadness(a, b) {
+    // Ready, get set, GO!!!
+    const sumSquareArray= a.map(function(num){return num * num});
+
+
+
+}
+
+console.log(arrayMadness([4, 5, 6], [1, 2, 3]))
