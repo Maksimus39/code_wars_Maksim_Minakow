45cbe2c014fbee4833dad0b2e7f368b67473ea5d
Macsim Minakov <maksim.minakov.83@mail.ru>
2025-02-07 14:38:22 +0300

    Создавая игру типа zork, вы создаете объект комнат. К сожалению, игра не работает. Найдите все ошибки в объекте комнат, чтобы ваша игра снова заработала.

---
+++ 8 kyu/main7.js
@@ -0,0 +1,20 @@
+// Создавая игру типа zork, вы создаете объект комнат. К сожалению, игра не работает. Найдите все ошибки в объекте комнат, чтобы ваша игра снова заработала.
+
+
+let rooms = {
+    first: {
+        description: 'This is the first room',
+        items: {
+            chair: 'The old chair looks comfortable',
+            lamp: 'This lamp looks ancient'
+        },
+    },
+    second: {
+        description: 'This is the second room',
+        items: {
+            couch: 'This couch looks like it would hurt your back',
+            table: 'On the table there is an unopened bottle of water'
+        }
+    }
+}
+
