a0bc1fa9ee6b83fb59fe5a4aea89de5ef5125f22
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-02-12 15:22:09 -0800

    Нахождение минимального и максимального числа в массиве.

--- 7 kyu/main4.js
+++ 7 kyu/main4.js
@@ -1,1 +1,16 @@
-// новая задача codeWars
+// Реализуйте функцию, которая возвращает минимальное и максимальное значение списка (в этом порядке).
+
+function getMinMax(arr) {
+    //..
+    const minNumberArray = arr.reduce((min,current) => {
+        return current < min? current : min;
+    },Infinity)
+
+    const maxNumberArray=arr.reduce((max,current) => {
+        return current > max ? current : max;
+    })
+
+    return [minNumberArray,maxNumberArray]
+}
+
+console.log(getMinMax([1, 2, 3, 4, 5, 6,9]))
