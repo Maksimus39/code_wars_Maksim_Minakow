bb3b7be7031d5b28add22e787d9ef7feb777d945
Max <maksim.minakov.83@mail.ru>
2025-02-25 09:50:36 +0300

    prototype

---
+++ 8 kyu/main22.js
@@ -0,0 +1,22 @@
+// Научите Снупи и Скуби-Ду лаять, используя методы объекта. В настоящее время лаять может только Снупи, а не Скуби-Ду.
+//
+// снупи.кора(); // возвращаем "Гав"
+// Скубиду.кора(); // неопределенный
+// Используйте прототипы методов, чтобы все собаки могли лаять.
+
+function Dog (breed) {
+    this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+    return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+
+scoobydoo.bark = function() {
+    return "Woof";
+}
