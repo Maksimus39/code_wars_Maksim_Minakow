91d6004eedd4f40d1430aa92bdf6a97e67adeac9
Max <maksim.minakov.83@mail.ru>
2025-04-04 13:05:07 +0300

    Если есть несколько самых длинных слов, вернуть самое длинное правое слово.

--- 7 kyu/main12.js
+++ 7 kyu/main12.js
@@ -1,1 +1,25 @@
-// 
+// Если задана строка слов, разделенных пробелами, вернуть самое длинное слово.
+// Если есть несколько самых длинных слов, вернуть самое длинное правое слово.
+//
+// Примеры
+// "red white blue" => "white"
+// "red blue gold" => "gold"
+
+
+function longestWord(stringOfWords) {
+    // Give me back the longest word!
+    const words = stringOfWords.split(' ');
+    let longest = '';
+
+    for (const word of words) {
+        if (word.length >= longest.length) {
+            longest = word;
+        }
+    }
+
+    return longest;
+}
+
+console.log(longestWord("lonely"))
+console.log(longestWord("one two three"))
+console.log(longestWord("red blue grey"))
