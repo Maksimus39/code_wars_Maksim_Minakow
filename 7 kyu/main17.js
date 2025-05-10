3711f67d798e45b16150417e95c960dd47521642
Max <maksim.minakov.83@mail.ru>
2025-05-10 13:45:08 +0300

    Скажите привет! refactoring

--- 7 kyu/main17.js
+++ 7 kyu/main17.js
@@ -12,12 +12,13 @@
 
 
 function greet(name) {
-    if (name === "") {
-        return null
-    } else if (name === null) {
-        return null
-    } else {
-        return `hello ${name}!`
+    switch (name) {
+        case "":
+            return null
+        case null:
+            return null
+        default:
+            return `hello ${name}!`
     }
 }
 
